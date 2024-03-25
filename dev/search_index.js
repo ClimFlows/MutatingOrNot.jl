b9b9b20f60d2ba6a84c4a602c07f34ad3c087864
Documenter.jl <documenter@juliadocs.github.io>
2024-03-25 21:38:44 +0000

    build based on dd29570

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MutatingOrNot","category":"page"},{"location":"#MutatingOrNot","page":"Home","title":"MutatingOrNot","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MutatingOrNot.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MutatingOrNot]","category":"page"}]
+}
