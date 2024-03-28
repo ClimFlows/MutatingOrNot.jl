6b2f054ed72f432ad9ac92ef29e2639b71921b4d
Thomas Dubos <thomas.dubos@lmd.ipsl.fr>
2024-03-28 18:04:47 +0100

    @inline

--- src/MutatingOrNot.jl
+++ src/MutatingOrNot.jl
@@ -7,11 +7,11 @@ const void = BasicVoid()
 
 Base.show(io::IO, ::Void) = print(io, "void")
 
-Base.getproperty(::Void, ::Symbol) = void
-Base.getindex(::Void, args...) = void
-Base.iterate(::Void, state = nothing) = (void, nothing)
+@inline Base.getproperty(::Void, ::Symbol) = void
+@inline Base.getindex(::Void, args...) = void
+@inline Base.iterate(::Void, state = nothing) = (void, nothing)
 
-Broadcast.materialize!(::Void, bc::Broadcast.Broadcasted) = Broadcast.materialize(bc)
+@inline Broadcast.materialize!(::Void, bc::Broadcast.Broadcasted) = Broadcast.materialize(bc)
 
 #========== for Julia <1.9 ==========#
 
