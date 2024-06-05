f4bbf67b2c8d2ab79365ecab4d32e88c3c0df67a
Thomas Dubos <thomas.dubos@lmd.ipsl.fr>
2024-06-05 19:12:34 +0200

    nothing[void]

--- src/MutatingOrNot.jl
+++ src/MutatingOrNot.jl
@@ -7,11 +7,14 @@ const void = BasicVoid()
 
 Base.show(io::IO, ::Void) = print(io, "void")
 
-@inline Base.getproperty(::Void, ::Symbol) = void
-@inline Base.getindex(::Void, args...) = void
-@inline Base.iterate(::Void, state = nothing) = (void, nothing)
+@inline Base.getproperty(v::Void, ::Symbol) = v
+@inline Base.getindex(v::Void, args...) = v
+@inline Base.iterate(v::Void, state = nothing) = (v, nothing)
 
 @inline Broadcast.materialize!(::Void, bc::Broadcast.Broadcasted) = Broadcast.materialize(bc)
+
+# for broadcasting with ManagedLoops
+Base.getindex(::Nothing, v::Void) = v
 
 #========== for Julia <1.9 ==========#
 
