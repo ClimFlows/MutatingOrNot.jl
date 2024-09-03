e8d9afcd7a3ad23507b6b809e9a9948301862727
Thomas Dubos <prenom.nom@lmd.ipsl.fr>
2024-09-03 18:06:05 +0200

    Add `similar(y,x)`

--- src/MutatingOrNot.jl
+++ src/MutatingOrNot.jl
@@ -13,9 +13,28 @@ Base.show(io::IO, ::Void) = print(io, "void")
 
 @inline Broadcast.materialize!(::Void, bc::Broadcast.Broadcasted) = Broadcast.materialize(bc)
 
-# for broadcasting with ManagedLoops
+# used by ManagedLoops for broadcasting
 Base.getindex(::Nothing, v::Void) = v
 
+"""
+    xx = similar(y, x)
+
+Convenience function that replaces more concisely:
+    
+    if x::Void
+        xx = similar(y)
+    else
+        xx = x
+    end
+
+The goal is to allocate `xx` only when a pre-allocated `x` is not provided. Furthermore `similar(y)` applies recursively to tuples and named tuples.
+Contrary to `Base.similar`, it is not possible to specify `eltype` or `dims`.
+"""
+similar(_,y) = y
+similar(x,::Void) = similar(x)
+similar(x::Union{Tuple, NamedTuple}) = map(similar, x)
+similar(x) = Base.similar(x)
+    
 #========== for Julia <1.9 ==========#
 
 using PackageExtensionCompat
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,21 @@
-using MutatingOrNot: void
+using MutatingOrNot: void, similar
 using Test
 using Zygote: Zygote
 using ForwardDiff: ForwardDiff
 
-f!(y, x) = @. y=x^2
+@inline f!(y, x) = @. y=x^2
 g(x) = sum(f!(void, x))
+
+is_similar(x::T, y::T) where T = (axes(x)==axes(y))
 
 @testset "MutatingOrNot.jl" begin
     let (x,y) = void
         @test void[1] == void
         @test void.prop == void
         @test (x,y) == (void,void)
+        u, v = randn(10), randn(10)
+        @test is_similar(u, similar(u, void))
+        @test similar(u, v) === v
     end
     let x = randn(10), y = similar(x)
         @test f!(y,x) == f!(void,x)
