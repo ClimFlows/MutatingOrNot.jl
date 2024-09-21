a254cda3e42b6a8ca1c370fa42d044acd80f324c
Thomas Dubos <thomas.dubos@lmd.ipsl.fr>
2024-09-21 23:28:56 +0200

    Introduce `similar!` and `dryrun`

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using MutatingOrNot: void, similar
+using MutatingOrNot: void, dryrun, has_dryrun, similar!
 using Test
 using Zygote: Zygote
 using ForwardDiff: ForwardDiff
@@ -8,19 +8,24 @@ g(x) = sum(f!(void, x))
 
 is_similar(x::T, y::T) where T = (axes(x)==axes(y))
 
+@info "show" void dryrun
+
 @testset "MutatingOrNot.jl" begin
     let (x,y) = void
         @test void[1] == void
         @test void.prop == void
         @test (x,y) == (void,void)
         u, v = randn(10), randn(10)
-        @test is_similar(u, similar(u, void))
-        @test similar(u, v) === v
+        @test is_similar(u, similar!(void, u))
+        @test similar!(v, u) === v
     end
     let x = randn(10), y = similar(x)
-        @test f!(y,x) == f!(void,x)
+        @test f!(x,y) == f!(void,x)
+        @test is_similar(f!(void,x), f!(dryrun,x))
         @test (@allocated f!(y,x)) == 0
         @test Zygote.gradient(g, x)[1] ≈ 2x
         @test ForwardDiff.gradient(g, x) ≈ 2x
+        @test has_dryrun(dryrun)
+        @test has_dryrun(dryrun)
     end
 end
